b4d55983576e12b8f2c7264e1861996898356334
SamuelPorcel <samuelsuasquita@gmail.com>
2025-03-17 17:33:18 -0300

    Atualização - Produção e Vendas
    Início da tela

---
+++ app/producao_e_vendas/page.js
@@ -0,0 +1,36 @@
+import "./producao_e_vendas.css"
+
+
+function Producao_e_vendas() {
+    
+    
+    
+    return ( 
+
+        <div>
+
+            <p className="voltar"> Voltar </p>
+              
+            <img className="logo"src="https://placehold.co/200"/>
+
+            <div>
+
+                <p>PRODUZIDOS</p>
+
+                <p>VENDAS</p>
+
+            </div>
+
+            <div>
+
+                <p className="caixa_de_texto"></p>
+
+                <p className="caixa_de_texto"></p>
+
+            </div>
+        </div>
+
+     );
+}
+
+export default Producao_e_vendas;
