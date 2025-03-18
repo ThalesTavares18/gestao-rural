409bbdc3d329031fda7877aea6620685f28f0c47
SamuelPorcel <samuelsuasquita@gmail.com>
2025-03-18 17:07:59 -0300

    Início v2
    atualização da página inicial

--- app/page.js
+++ app/page.js
@@ -6,30 +6,41 @@ function Inicio () {
     
     
     return ( 
-
+        
         <div>
-            <img className="logo"src="https://placehold.co/200"/>
-
+            
                  <div className="espacamento">
 
-                    <div>
+                    <img className="logo"src="https://placehold.co/300"/>
 
-                        <a href="http://localhost:3000/producao_e_vendas"> <p className="botoes">Produção e Vendas </p> </a>
-                        <a href="http://localhost:3000/estoque"> <p className="botoes"> Estoque </p> </a>
+                    <div className="espacamento_cima">
+
+                        <div className="espacamento">
+
+                            <a href="http://localhost:3000/producao_e_vendas"> <p className="botoes">Produção e Vendas </p> </a>
+                            <a href="http://localhost:3000/estoque"> <p className="botoes"> Estoque </p> </a>     
+
+                        </div>
+
+                        <div className="espacamento">
+
+                            <a href="http://localhost:3000/preparacao"> <p className="botoes"> Preparação </p> </a>
+                            <a href="http://localhost:3000/Financeiro"> <p className="botoes"> Financeiro </p> </a>
+
+                        </div>
+
+                    <div className="espacamento_esquerda">
+
+                        <a href="http://localhost:3000/meus_clientes"> <p className="botoes"> Meus Clientes </p> </a>
 
                     </div>
 
-                    <div>
-
-                        <a href="http://localhost:3000/preparacao"> <p className="botoes"> Preparação </p> </a>
-                        <a href="http://localhost:3000/financeiro"> <p className="botoes"> Financeiro </p> </a>
 
                     </div>
 
+                </div>
 
-                 </div>
 
-                <a href="http://localhost:3000/meus_clientes"> <p style={{marginLeft:810, marginTop:0}} className="botoes"> Meus Clientes </p> </a>
 
         </div>
      );
