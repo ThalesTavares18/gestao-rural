1df285705d1fc7d36ae25b57ac001620830a9dbc
JayneLisboa <jaynelins15@gmail.com>
2025-03-18 16:15:34 -0300

    atualizando

---
+++ app/MeusClientes/page.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function MeusClientes() {
+
+    const [ usuario, alteraUsuario]= useState(true)
+    const [ telefone, alteraTelefone]= useState(true)
+    const [ email, alteraEmail]= useState(true)
+
+    function Cadastro (){
+
+        if( usuario == true ){
+
+            
+        }
+    }
+    return ( 
+
+
+     );
+}
+
+export default MeusClientes;
