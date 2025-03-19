30cabb010f7c5fcf53dd05a0516a30928cfaf4cf
SamuelPorcel <samuelsuasquita@gmail.com>
2025-03-19 16:29:07 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,21 +18,21 @@ function Inicio () {
 
                         <div className="espacamento">
 
-                            <a href="http://localhost:3000/producao_e_vendas"> <p className="botoes">Produção e Vendas </p> </a>
-                            <a href="http://localhost:3000/estoque"> <p className="botoes"> Estoque </p> </a>     
+                            <a href="./producao_e_vendas"> <p className="botoes">Produção e Vendas </p> </a>
+                            <a href="./estoque"> <p className="botoes"> Estoque </p> </a>     
 
                         </div>
 
                         <div className="espacamento">
 
-                            <a href="http://localhost:3000/preparacao"> <p className="botoes"> Preparação </p> </a>
-                            <a href="http://localhost:3000/Financeiro"> <p className="botoes"> Financeiro </p> </a>
+                            <a href="./preparacao"> <p className="botoes"> Preparação </p> </a>
+                            <a href="./Financeiro"> <p className="botoes"> Financeiro </p> </a>
 
                         </div>
 
                     <div className="espacamento_esquerda">
 
-                        <a href="http://localhost:3000/MeusClientes"> <p style={{marginLeft:30}} className="botoes"> Meus Clientes </p> </a>
+                        <a href="./MeusClientes"> <p style={{marginLeft:30}} className="botoes"> Meus Clientes </p> </a>
                         
                     </div>
 
