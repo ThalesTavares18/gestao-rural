f71e12e9bb02ebaaade6e1179dd9051c9ebb7b8e
JayneLisboa <jaynelins15@gmail.com>
2025-04-08 17:28:31 -0300

    aa

--- app/MeusClientes/page.js
+++ app/MeusClientes/page.js
@@ -1,206 +1,138 @@
 'use client'
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-import "./meusclientes.css"
+import "./meusclientes.css";
 
 export default function MeusClientes() {
+    const [usuario, alteraUsuario] = useState([]);
+    const [nome, alteraNome] = useState([]);
+    const [telefone, alteraTelefone] = useState([]);
 
-    const [ usuario, alteraUsuario]= useState([])
-    const [ telefone, alteraTelefone]= useState([])
-    
-    const [ mostraUsuario, alteraMostraUsuario ]= useState(false)
-    const [ mostraPesquisa, alteraMostraPesquisa ]= useState(false)
-    
+    const [mostraPesquisa, alteraMostraPesquisa] = useState(false);
+    const [cadastroContato, alteraCadastroContato] = useState(0);
 
-
-
-    async function buscaTodos(){
-        const response= await axios.get("http://localhost:3000/api/meus_clientes")
-        alteraUsuario(response.data)
-        console.log(response.data)
-    
+    async function buscaTodos() {
+        const response = await axios.get("http://localhost:3000/api/meus_clientes");
+        alteraUsuario(response.data);
+        console.log(response.data);
     }
 
-    async function insereUsuario(){
-
-        e.preventDefault()
+    async function insereUsuario(e) {
+        e.preventDefault(); // Garante que o formulário não será enviado de forma tradicional
 
         const obj = {
-            nome: usuario,
-            contato: telefone
+            nome: nome,
+            contato: telefone,
         }
 
-        const response= await axios.post("http://localhost:3000/api/meus_clientes", obj)
-        console.log(response)
+        const response = await axios.post("http://localhost:3000/api/meus_clientes", obj);
+        console.log(response);
 
         buscaTodos()
     }
 
-    function enviaFormulario(e){
-        e.preventDefault()
-
-        if( editando == 0){
-            insereProdutos()
-        
+    function enviaFormulario(e) {
+        e.preventDefault();
+        if (cadastroContato == 0) {
+            insereUsuario(e);
         }
+    else{
+        atualizaProduto()
     }
+}
 
-    function Cadastro (){
+    useEffect(() => {
+        buscaTodos();
+    }, []);
 
-        if( usuario && (telefone || email)){
-            
-            alteraMostraUsuario(true)
-            alteraMostraUsuario = alteraMostraPesquisa
-        }else{
-
-
-
-        }
-
-    }
-
-    useEffect( ()=> {
-        buscaTodos()
-       
-    }, [])
-
-
-    
-    
-
-   
-    
-    
-    
-    
-    
-    
-    return ( 
-
+    return (
         <div>
-
-            <a href=" http://localhost:3000/"> 
-
+            <a href="http://localhost:3000/">
                 <button className="buttonVoltar"> Voltar </button>
-            
             </a>
-            <br/>
-            <br/>
+            <br />
+            <br />
 
-
-            <img className="logo"src="logo.png" width={100} height={100}/>
-
+            <img className="logo" src="logo.png" width={100} height={100} />
 
             <div className="MenuClientes">
-
                 <h1> Meus Clientes</h1>
-
             </div>
 
-            <div> 
-
-                <input onChange={ (e)=> mostraPesquisa (e.target.value)}/>
-
-                <button className="buttonSalvar"onClick={ ()=> mostraPesquisa()}> Pesquisar </button>
-
-                <hr/>
-
-
-
-
+            <div>
+                <input onChange={(e) => alteraMostraPesquisa(e.target.value)} />
+                <button className="buttonSalvar" onClick={() => alteraMostraPesquisa()}> Pesquisar </button>
+                <hr />
             </div>
 
             <div>
                 <style>
-                        {`
-                        table {
-                            width: 100%;
-                            border-collapse: collapse;
-                            margin: 20px 0;
-                            font-family: 'Arial', sans-serif;
-                        }
-                        th, td {
-                            border: 1px solid #ddd;
-                            padding: 12px;
-                            text-align: left;
-                            font-size: 16px;
-                        }
-                        th {
-                            background-color: #4CAF50;
-                            color: white;
-                        }
-                        tr:nth-child(even) {
-                            background-color: #f2f2f2;
-                        }
-                        tr:hover {
-                            background-color: #ddd;
-                        }
-                        `}
+                    {`
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                        margin: 20px 0;
+                        font-family: 'Arial', sans-serif;
+                    }
+                    th, td {
+                        border: 1px solid #ddd;
+                        padding: 12px;
+                        text-align: left;
+                        font-size: 16px;
+                    }
+                    th {
+                        background-color: #4CAF50;
+                        color: white;
+                    }
+                    tr:nth-child(even) {
+                        background-color: #f2f2f2;
+                    }
+                    tr:hover {
+                        background-color: #ddd;
+                    }
+                    `}
                 </style>
-                
+
                 <h2> Lista Contatos </h2>
 
-                {
-                    usuario.length > 0 ?
-                        <table>
-
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nome</th>
-                                    <th>Contato</th>
+                {usuario.length > 0 ? (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Contato</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {usuario.map((i, index) => (
+                                <tr key={index}>
+                                    <td>{i.id}</td>
+                                    <td>{i.nome}</td>
+                                    <td>{i.contato}</td>
                                 </tr>
-                                
-                            </thead>
-                            <tbody>
-
-                                {
-                                    usuario.map ((i, index) =>
-                                        <tr key={index}>
-                                            <td>{i.id}</td>
-                                            <td>{i.nome}</td>
-                                            <td>{i.contato}</td>
-                                        </tr>
-                                    )
-                                }
-
-                            </tbody>
-
-
-                        </table>
-
-                    :
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
                     <p>Carregando...</p>
-
-                }
+                )}
             </div>
 
-
-          
-               <h1>  Cadastrar Clientes  </h1>
-                <br/>
+            <h1> Cadastrar Clientes </h1>
+            <br />
 
             <div className="sumario">
-
                 <form onSubmit={(e)=> enviaFormulario (e)}>
+                    <label> Digite o nome: <br /> <input onChange={(e) => alteraNome(e.target.value)} /> </label>
+                    <br />
+                    <label> Digite o contato: <br /> <input onChange={(e) => alteraTelefone(e.target.value)} /> </label>
+                    <br />
+                    <br />
 
-                    <label> Digite o nome: <br/> <input onChange={ (e)=> alteraUsuario (e.target.value) } /> </label>
-                    <br/>
-                    <label> Digite o contato: <br/> <input onChange={ (e)=> alteraTelefone (e.target.value) } /> </label>
-                    <br/>
-                    <br/>
-                    
-                    <button className="buttonSalvar"onClick={ ()=> Cadastro()}> Salvar </button>
-
+                    <button className="buttonSalvar" type="submit" >Salvar</button>
                 </form>
             </div>
-            
-            
-
         </div>
-
-
-     );
-    }
-
+    );
+}
--- app/api/meus_clientes/route.js
+++ app/api/meus_clientes/route.js
@@ -2,43 +2,33 @@ import conexao from "@/app/lib/conexao";
 
 export async function GET(){
 
-    
-    
-    const query = `SELECT * FROM contatos;`
-    const [usuario] = await conexao.execute(query)
+    const query = `SELECT * FROM contatos`
+    const [results] = await conexao.execute(query)
 
     return new Response(
-
-           JSON.stringify(usuario),
-        {   
-            status: 200,
-            headers: {"content-type": "application/json"}
+        JSON.stringify(results),
+        {
+            status:200,
+            headers: { "Content-Type": "application/json"}
         }
-
     )
-}
+   
 
+}
 
 export async function POST( request ){
 
     const body = await request.json()
-    
-    const query = ` 
-        INSERT INTO contatos
-        (nome, contato)
-        VALUES
-        (?,?);
 
+    const query = `
+        INSERT INTO produtos (nome, contato) VALUES (?,?);
     `
-    const [results] = await conexao.execute(
-        query,
-        [body.nome, body.contato]
 
+    const [results] = await conexao.execute(
+        query, 
+        [body.nome, body.contato]
     )
 
-    return new Response( JSON.stringify ( results.insertId ))
-
-
+    return new Response(JSON.stringify(results.insertId))
 
 }
-    
