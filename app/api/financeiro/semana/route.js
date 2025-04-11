fae26b49dc41b1d94afd936527446f89d1ddaa67
thalesilvaaa <thales.htsilva@senacsp.edu.br>
2025-04-11 17:28:37 -0300

    thales

--- app/api/financeiro/semana/route.js
+++ app/api/financeiro/semana/route.js
@@ -7,10 +7,8 @@ export async function GET( request){
 
     seteDiasAtras.setDate(hoje.getDate() - 7);
 
-    const query = `SELECT produtos.nome, vendas.quantidade, (produtos.preco * vendas.quantidade) AS total_venda, vendas.data 
-            FROM produtos
-            JOIN vendas ON vendas.id_produto = produtos.id
-            WHERE DATE(data) >= ? AND DATE(data) <= ?;`
+    const query = `SELECT * FROM estoque WHERE entrada ='0'
+            AND DATE(data) >= ? AND DATE(data) <= ?;`
     const [results] = await conexao.execute(query, [seteDiasAtras, hoje])
 
     return new Response(
