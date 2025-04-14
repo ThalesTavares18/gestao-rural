403f6b1aa57aa1d9ee3620ce9f01b5b6fb431ccf
leonardosr0206 <leodosanstos212325@gmail.com>
2025-04-14 14:20:31 -0300

    Update route.js

--- app/api/estoque/route.js
+++ app/api/estoque/route.js
@@ -3,53 +3,55 @@
 import conexao from "@/app/lib/conexao";
 
 export async function GET() {
-    const query = `
-        SELECT p.nome, p.preco, e.quantidade, e.id_produtoFROM estoque e JOIN produtos p ON e.id_produto = p.id;
-    `;
-    const [results] = await conexao.execute(query);
+    
 
-    return new Response(
-        JSON.stringify(results),
-        {
-            status: 200,
-            headers: { "Content-Type": "application/json" }
-        }
-    );
-}
+        const query = `
+            SELECT p.nome, p.preco, e.quantidade, e.id_produto FROM estoque e JOIN produtos p ON e.id_produto = p.id;
+        `;
+        const [results] = await conexao.execute(query);
 
-export async function POST(request) {
-    const body = await request.json();
-    const query = `
-        INSERT INTO estoque (id_produto, quantidade)
-        VALUES ((SELECT id FROM produtos WHERE nome = ?), ?);
-    `;
+        return new Response(
+            JSON.stringify(results),
+            {
+                status: 200,
+                headers: { "Content-Type": "application/json" }
+            }
+      );
+    }
+    
+    export async function POST(request) {
+        const body = await request.json();
+        const query = `
+           INSERT INTO estoque( id_produto, quantidade, data, entrada) VALUES (?, ?, '?',?);
+        `;
 
-    const [results] = await conexao.execute(query, [body.id_produto, body.quantidade]);
+        const [results] = await conexao.execute(query, [body.id_produto, body.quantidade]);
 
-    return new Response(
-        JSON.stringify({ id: results.insertId }),
-        {
-            status: 200,
-            headers: { "Content-Type": "application/json" }
-        }
-    );
-}
+        return new Response(
+            JSON.stringify({ id: results.insertId }),
+            {
+                status: 200,
+                headers: { "Content-Type": "application/json" }
+            }
+        );
+    }
 
-export async function UPDATE(request) {
-    const body = await request.json();
-    const query = `
-        UPDATE estoque SET id_produto =1, quantidade =20 WHERE id_produto = 1`;
+    export async function PUT(request) {
+        const body = await request.json();
+        const query = `
+            UPDATE estoque SET id_produto =1, quantidade =20 WHERE id_produto = 1`;
 
 
-    const [results] = await conexao.execute(query, [body.quantidade, body.id_produto]);
+        const [results] = await conexao.execute(query, [body.quantidade, body.id_produto]);
 
-    return new Response(
-        JSON.stringify({ message: "Estoque atualizado com sucesso!" }),
-        {
-            status: 200,
-            headers: { "Content-Type": "application/json" }
-        }
-    );
+        return new Response(
+            JSON.stringify({ message: "Estoque atualizado com sucesso!" }),
+            {
+                status: 200,
+                headers: { "Content-Type": "application/json" }
+            }
+        );
+    
 }
 
 
