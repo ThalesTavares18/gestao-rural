b691f8c20c608f2084c846f32fe7f16e219ab03b
SamuelPorcel <samuelsuasquita@gmail.com>
2025-04-16 14:25:00 -0300

    sla

--- app/api/producao_e_vendas/vendas/route.js
+++ app/api/producao_e_vendas/vendas/route.js
@@ -2,7 +2,7 @@ import conexao from "@/app/lib/conexao";
 
 export async function GET(){
 
-    const query = `SELECT produtos.nome, vendas.quantidade FROM produtos JOIN vendas ON vendas.id_produto = produtos.id;`
+    const query = `SELECT produtos.nome, estoque.quantidade, estoque.entrada FROM estoque JOIN produtos ON estoque.id_produto = produtos.id WHERE estoque.entrada = 1;`
     const [results] = await conexao.execute(query)
 
     return new Response(
