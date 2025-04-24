daed2d50a28a2e0454055f9b7e33b408867c1618
SamuelPorcel <samuelsuasquita@gmail.com>
2025-04-24 17:13:16 -0300

    Inserção de produtos e estoque funcionando

--- app/api/estoque/route.js
+++ app/api/estoque/route.js
@@ -15,30 +15,75 @@ export async function GET(){
 
 }
 
-export async function POST( request ){
+export async function POSTasdasd( request ){
 
     const body = await request.json()
 
     const query = `
-        START TRANSACTION;
+            INSERT INTO estoque 
+            (id_produto, quantidade, entrada)
+            VALUES 
+            (77, ?, 1);
+    `
+    
+    const [results] = await conexao.execute(
+        query,
+        [body.quantidade]
+    )
+
+    return new Response( JSON.stringify(results.insertId) )
+
+}
+
+export async function POST( request ){
+
+    const body = await request.json()
+
+    const id_inserido =  await insereProduto(body);
+    const resultado = await insereEstoque(id_inserido, body.quantidade);
+
+    return new Response( JSON.stringify(id_inserido) )
+
+}
+
+async function insereProduto( item ){
+    const query = `
+
 
             INSERT INTO produtos
             (nome, preco, quantidade)
             VALUES
             (?, ?, ?);
 
-            INSERT INTO estoque 
-            (id_produto, quantidade, entrada)
-            VALUES 
-            (LAST_INSERT_ID(), ?, 1)
-
-        COMMIT;
     `
+    
     const [results] = await conexao.execute(
         query,
-        [body.nome, body.preco, body.quantidade, body.quantidade]
+        [item.nome, item.preco, item.quantidade]
     )
 
-    return new Response( JSON.stringify(results.insertId) )
+    console.log("Item de ID "+results.insertId+" inserido com sucesso!")
+
+    return results.insertId
+
+}
+
+async function insereEstoque(id, quantidade){
+    const query = `
+        INSERT INTO estoque 
+        (id_produto, quantidade, entrada)
+        VALUES 
+        (?, ?, 1)
+
+    `
+
+    const [results] = await conexao.execute(
+        query,
+        [id, quantidade]
+    )
+
+    console.log("Estoque inserido com id do produto "+id+" e quantidade "+quantidade)
+
+    return results;
 
 }
